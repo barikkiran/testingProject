0865e85bf7acc3a9a13d580c9c5d0d9bf2f9a22f
pintu <kirankumarbarik1994@gmail.com>
2021-12-07 11:14:05 +0530

    adding new attributes into TestDemo class

--- TestingProject/src/com/testing/app/TestDemo.java
+++ TestingProject/src/com/testing/app/TestDemo.java
@@ -6,6 +6,9 @@ package com.testing.app;
  */
 public class TestDemo {
 
+	private int t1;
+	private int t2;
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Good morning everyone...!");
